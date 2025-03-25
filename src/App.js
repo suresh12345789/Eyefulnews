ce6032667371b87063ef05c4e3b86153485e5bc4
suresh12122121 <chittirisuresh@gmail.com>
2025-03-25 10:32:03 +0530

    changes language

--- src/App.js
+++ src/App.js
@@ -5,38 +5,27 @@ import ForBusinesses from "./Components/Home/ForBusinesses";
 import Banner from "./Components/Home/Banner";
 import ContactUs from "./Components/Home/ContactUs";
 import Download from "./Components/Home/Download";
-<<<<<<< Updated upstream
-import FAQS from "./Components/Home/FAQS";
-import Language from "./Components/Home/Language";
-=======
-import Fotter from "./Common/Fotter"
-import Languages from "./Components/Languages";
 
->>>>>>> Stashed changes
+import FAQS from "./Components/Home/FAQS";
+import Language from "./Components/Home/Language"
+import Fotter from "./Common/Fotter"
+
+
 
 export default function App() {
   return (
     <div className="">
       <BrowserRouter>
         <Navbar />
-<<<<<<< Updated upstream
         <Banner />
-=======
-        <Banner/>
-        <Languages/>
->>>>>>> Stashed changes
         <Categorys />
         <ForBusinesses />
-<<<<<<< Updated upstream
         <FAQS />
         <Download />
         <Language/>
         <ContactUs />
-=======
-        <ContactUs/>
         <Download/>
-<Fotter/>
->>>>>>> Stashed changes
+        <Fotter/>
       </BrowserRouter>
     </div>
   );
